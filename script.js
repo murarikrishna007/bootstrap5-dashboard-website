f0ff9738918f01a7c5ddf870bd02b501ee559021
murarikrishna007 <murarikrishna0071994@gmail.com>
2022-04-08 16:57:20 +0530

    bootstrap5 dashboard build

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// console.log("krishna murari");
+
+let menuBar = document.getElementById("wrapper");
+let togggleButton = document.getElementById("menu-toggle");
+
+togggleButton.onclick = function (){
+    menuBar.classList.toggle("toggled");
+}
